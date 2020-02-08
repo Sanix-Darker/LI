dd9972addf76d0d657d19ca9d5eca71d9f2cef7b
sanix-darker <s4nixd@gmail.com>
2020-02-08 18:27:03 +0100

    [2]Updates

--- lino.py
+++ lino.py
@@ -1,15 +1,13 @@
-from li import Li
+from li import Li, KEYWORDS
 from pyfirmata import Arduino
 import time
 
-KEYWORDS = {
+LINO_KEYWORDS = {
     "lang": {
-        'open': 'open', 'read': 'read', 'write': 'write', 'close': 'close',
-        '+': '+', '-': '-', '*': '*', '/': '/', 'print': 'affiche',
-        'println': 'affiche_xa', 'scanf': 'demande', '=': '=', '!': '!', '<': '<',
-        '>': '>', '<=': '<=', '>=': '>=', 'len': 'taille', 'ins': 'ins', 'del': 'supr',
-        'cut': 'cut', 'map': 'map', 'fold': 'fold', 'filter': 'filter', 'assert': 'assert',
-        'round': 'round', 'type': 'type', 'import': 'import'
+        **KEYWORDS,
+        **{
+            'digital_up': 'allumes', 'digital_down': 'eteinds', 'wait': 'attends'
+        }
     }
 }
 
@@ -17,7 +15,7 @@ KEYWORDS = {
 class Lino(Li):
     def __init__(self, output='/dev/ttyACM0', lang="lang", keywords=None, ):
         if keywords is None:
-            keywords = KEYWORDS
+            keywords = LINO_KEYWORDS
         super().__init__()
         self.lang = lang
         self.output = output
